8045adaea75ef5f4c997a3d76eba7a212ecd855a
jaysonAviso <jayson.aviso1@gmail.com>
2022-09-25 12:38:52 +0800

    Applied fix

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -24,7 +24,39 @@ namespace API.Extensions
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(opt =>
             {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+                AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                string connStr;
+
+                //Depending on if in devlopment or production, use either heroku-provided
+                //connection string, development connection string from env var.
+                if (env == "Development")
+                {
+                    //use connection string from file.
+                    connStr = config.GetConnectionString("DefaultConnection");
+                }
+                else
+                {
+                    //Use connection string provided at runtime by Heroku
+                    var connURL = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                    //Parse connection URL to connection string for npgsql
+                    connURL = connURL.Replace("postgres://", string.Empty);
+                    var pgUserPass = connURL.Split("@")[0];
+                    var pgHostPortDb = connURL.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+
+                    connStr = $"User ID={pgUser};Password={pgPass};Host={pgHost};Port={pgPort};Database={pgDb}";
+                }
+
+                //Whether the connection came from the local development configuration file or from the environment variable from Heroku, use it to set up your DbContext.
+                opt.UseNpgsql(connStr);
             });
             return services;
         }
--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -7,6 +7,7 @@ import { environment } from 'src/environments/environment';
 import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
+import { BusyService } from './busy.service';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 @Injectable({
@@ -19,17 +20,20 @@ export class MessageService {
   private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private busyService: BusyService) { }
 
   createHubConnection(user: User, recipientUsername: string){
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl + 'message?user=' + recipientUsername, {
         accessTokenFactory: () => user.token
       })
       .withAutomaticReconnect()
       .build();
-
-    this.hubConnection.start().catch(err => console.log(err));
+      
+    this.hubConnection.start()
+      .catch(err => console.log(err))
+      .finally(() => this.busyService.idle());
 
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
@@ -57,6 +61,7 @@ export class MessageService {
 
   stopHubConnection() {
     if(this.hubConnection) {
+      this.messageThreadSource.next([]);
       this.hubConnection.stop();
     }
   }
